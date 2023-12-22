47b53019f8606f608bdb92cea3b3e824d0acfc91
Michael Lam <mlamchamkee@gmail.com>
2023-12-21 21:41:19 -0500

    updates template

--- 2023/dayx.py
+++ 2023/dayx.py
@@ -2,6 +2,25 @@ def solution():
     pass
 
 
+def solution2():
+    pass
+
+
 if __name__ == "__main__":
-    print("Solution 1: ", solution())
-    print("Solution 2: ", solution2())
+    BASE_PATH = "/Users/mlamchamkee/dev/advent-of-code/2023/"
+
+    """Unit Tests"""
+
+    example1 = solution()
+    print("Example 1: ", example1)
+    assert example1 == 999, "Test Example 1"
+
+    solution1 = solution()
+    print("Solution 1: ", solution1)
+
+    example2 = solution2()
+    print("Example 2: ", example2)
+    assert example2 == 999, "Test Example 2"
+
+    solution2 = solution2()
+    print("Solution 2: ", solution2)
